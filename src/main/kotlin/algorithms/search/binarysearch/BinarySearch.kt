96d7b806c99eb9920c92065cc20137aa4179cf97
Navid Rojiani <nrojiani@gmail.com>
2018-09-14 01:20:09 -0400

    binary search

--- src/main/kotlin/algorithms/search/binarysearch/BinarySearch.kt
+++ src/main/kotlin/algorithms/search/binarysearch/BinarySearch.kt
@@ -7,36 +7,15 @@ import extensions.lists.isSorted
  * BinarySearch
  */
 
-/**
- * Returns an [Int?] representing the index (null if the target is not found)
- *
- * Time: `O(log n)`
- * Space: `O(1)`
- */
-fun <T : Comparable<T>> List<T>.binarySearch(key: T): Int? {
-    var indexSearchRange = 0..lastIndex
-
-    while (!indexSearchRange.isEmpty()) {
-        val mid = (indexSearchRange.start + indexSearchRange.endInclusive) / 2
-        indexSearchRange = when {
-            key < this[mid] -> (indexSearchRange.start until mid)
-            key > this[mid] -> (mid + 1..lastIndex)
-            else -> return mid
-        }
-    }
-
-    return null
-}
-
-
-/** Exercism Solution: http://exercism.io/exercises/kotlin/binary-search/readme */
 const val NOT_FOUND: Int = -1
 
-fun <T : Comparable<T>> binarySearchExercism(list: List<T>, searchKey: T): Int {
-    require(list.isSorted()) { "Binary Search can only be performed on a sorted list" }
-
+/**
+ * Time: `O(log n)`
+ * Space: `O(1)`
+ * @return the index of the element if found, or -1 if not found
+ */
+fun <T : Comparable<T>> binarySearch(list: List<T>, searchKey: T): Int {
     var range = 0..list.lastIndex
-
     while (!range.isEmpty()) {
         val mid = (range.start + range.endInclusive) / 2
         when {
--- src/test/kotlin/algorithms/search/binarysearch/BinarySearchTest.kt
+++ src/test/kotlin/algorithms/search/binarysearch/BinarySearchTest.kt
@@ -8,98 +8,46 @@ class BinarySearchTest {
 
     @Test
     fun binarySearch() {
-        assertEquals(1, listOf(1, 3, 4, 7, 9).binarySearch(3))
-    }
-
-
-    @Test
-    fun binarySearch_ShouldReturnNullForAValueNotPresentInTheList() {
-        assertNull(listOf(1, 2, 3, 4).binarySearch(5))
+        assertEquals(1, binarySearch(listOf(1, 3, 4, 7, 9), 3))
     }
 
     @Test
-    fun binarySearch_ListIsEmpty() {
-        assertNull(emptyList<Int>().binarySearch(7))
+    fun shouldReturnMinus1ForAValueNotPresentInTheList() {
+        assertEquals(-1, binarySearch(listOf(1, 2, 3, 4), 5))
     }
 
     @Test
-    fun binarySearch_ShouldFindTheItemInASingleElementList() {
-        assertEquals(0, listOf(3).binarySearch(3))
+    fun listIsEmpty() {
+        assertEquals(-1, binarySearch(emptyList(), 7))
     }
 
     @Test
-    fun binarySearch_ShouldFindTheFirstItemInATwoElementList() {
-        assertEquals(0, listOf(4, 11).binarySearch(4))
+    fun shouldFindTheItemInASingleElementList() {
+        assertEquals(0, binarySearch(listOf(3), 3))
     }
 
     @Test
-    fun binarySearch_ShouldFindTheLastItemInATwoElementList() {
-        assertEquals(1, listOf(4, 11).binarySearch(11))
+    fun shouldFindTheFirstItemInATwoElementList() {
+        assertEquals(0, binarySearch(listOf(4, 11), 4))
     }
 
     @Test
-    fun binarySearch_ShouldFindTheMidItemInAnOddLengthList() {
-        assertEquals(2, listOf(4, 4, 7, 9, 11).binarySearch(7))
+    fun shouldFindTheLastItemInATwoElementList() {
+        assertEquals(1, binarySearch(listOf(4, 11), 11))
     }
 
     @Test
-    fun binarySearch_ShouldFindThePreMidItemInAnEvenLengthList() {
-        assertEquals(1, listOf(4, 5, 9, 11).binarySearch(5))
+    fun shouldFindTheMidItemInAnOddLengthList() {
+        assertEquals(2, binarySearch(listOf(4, 4, 7, 9, 11), 7))
     }
 
     @Test
-    fun binarySearch_ShouldFindThePostMidItemInAnEvenLengthList() {
-        assertEquals(2, listOf(4, 5, 17, 29).binarySearch(17))
-    }
-
-    /* Exercism tests */
-    @Test(expected = IllegalArgumentException::class)
-    fun unsortedListCannotBeSearchedExercism() {
-        binarySearchExercism(listOf(1, 2, 4, 3), 1)
+    fun shouldFindThePreMidItemInAnEvenLengthList() {
+        assertEquals(1, binarySearch(listOf(4, 5, 9, 11), 5))
     }
 
     @Test
-    fun binarySearchExercism() {
-        assertEquals(1, binarySearchExercism(listOf(1, 3, 4, 7, 9), 3))
-    }
-
-    @Test
-    fun shouldReturnMinus1ForAValueNotPresentInTheListExercism() {
-        assertEquals(-1, binarySearchExercism(listOf(1, 2, 3, 4), 5))
-    }
-
-    @Test
-    fun listIsEmptyExercism() {
-        assertEquals(-1, binarySearchExercism(emptyList(), 7))
-    }
-
-    @Test
-    fun shouldFindTheItemInASingleElementListExercism() {
-        assertEquals(0, binarySearchExercism(listOf(3), 3))
-    }
-
-    @Test
-    fun shouldFindTheFirstItemInATwoElementListExercism() {
-        assertEquals(0, binarySearchExercism(listOf(4, 11), 4))
-    }
-
-    @Test
-    fun shouldFindTheLastItemInATwoElementListExercism() {
-        assertEquals(1, binarySearchExercism(listOf(4, 11), 11))
-    }
-
-    @Test
-    fun shouldFindTheMidItemInAnOddLengthListExercism() {
-        assertEquals(2, binarySearchExercism(listOf(4, 4, 7, 9, 11), 7))
-    }
-
-    @Test
-    fun shouldFindThePreMidItemInAnEvenLengthListExercism() {
-        assertEquals(1, binarySearchExercism(listOf(4, 5, 9, 11), 5))
-    }
-
-    @Test
-    fun shouldFindThePostMidItemInAnEvenLengthListExercism() {
-        assertEquals(2, binarySearchExercism(listOf(4, 5, 17, 29), 17))
+    fun shouldFindThePostMidItemInAnEvenLengthList() {
+        assertEquals(2, binarySearch(listOf(4, 5, 17, 29), 17))
     }
 }
