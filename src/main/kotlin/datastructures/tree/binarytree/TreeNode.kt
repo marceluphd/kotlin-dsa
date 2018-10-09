8a9d5025e3b0e032f45c1b48d234787c7f46ca9d
Navid Rojiani <nrojiani@gmail.com>
2018-10-08 21:00:43 -0400

    add Binary Tree

---
+++ src/main/kotlin/datastructures/tree/binarytree/TreeNode.kt
@@ -0,0 +1,409 @@
+package datastructures.tree.binarytree
+
+import datastructures.tree.binarytree.DFSTraversalOrder.*
+import java.util.*
+
+/**
+ * Definition for a binary tree node (based on LeetCode definition with some changes).
+ */
+class TreeNode(var value: Int = 0, var left: TreeNode? = null, var right: TreeNode? = null) {
+
+    val hasLeft: Boolean get() = left != null
+    val hasRight: Boolean get() = right != null
+    val isLeaf: Boolean get() = !hasLeft && !hasRight
+    val hasSingleChild: Boolean get() = (hasLeft || hasRight) && !(hasLeft && hasRight)
+    val hasTwoChildren: Boolean get() = left != null && right != null
+
+
+    /**
+     * Returns the number of nodes in the tree.
+     *
+     * **Time**: `O(n)`
+     *
+     * **Space**: `O(n)` (recursive stack space)
+     */
+    val size: Int get() = 1 + (left?.size ?: 0) + (right?.size ?: 0)
+
+    /**
+     * Height: the number of edges on the longest path between this node and a leaf.
+     *
+     * **Time**: `O(n)`
+     *
+     * **Space**: `O(n)`
+     */
+    val height: Int
+        get() {
+            if (isLeaf) return 0
+            return 1 + maxOf(left?.height ?: 0, right?.height ?: 0)
+        }
+
+
+    /**
+     * Returns the left and right children of this node as a [Pair] of nullable TreeNodes.
+     * ```
+     * val (l, r) = tree?.children
+     * ```
+     *
+     * **Time**: `O(1)`
+     *
+     * **Space**: `O(1)`
+     */
+    val children: Pair<TreeNode?, TreeNode?> get() = Pair(left, right)
+
+    /**
+     * Checks whether the specified value is contained in the tree.
+     *
+     * Usage:
+     * ```
+     * val tree: TreeNode = TreeNode(1)
+     * tree.left = TreeNode(2)
+     * tree.right = TreeNode(3)
+     * 2 in tree
+     * ```
+     *
+     * **Time**: `O(n)`
+     *
+     * **Space**: `O(n)`
+     */
+    operator fun contains(value: Int): Boolean = find(value) != null
+
+
+    override fun toString(): String = "($value)"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TreeNode
+
+        if (value != other.value) return false
+        if (left != other.left) return false
+        if (right != other.right) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = value
+        result = 31 * result + (left?.hashCode() ?: 0)
+        result = 31 * result + (right?.hashCode() ?: 0)
+        return result
+    }
+}
+
+/**
+ * Search for a value in the binary tree, returning the node
+ * containing the value if it is found, or else null.
+ *
+ * **Time**: `O(n)`
+ *
+ * **Space**: `O(n)`
+ */
+fun TreeNode?.find(searchValue: Int): TreeNode? {
+    this ?: return null
+
+    return when {
+        searchValue < value -> left?.find(searchValue)
+        searchValue > value -> right?.find(searchValue)
+        else -> this
+    }
+}
+
+
+sealed class DFSTraversalOrder {
+    object PREORDER : DFSTraversalOrder()
+    object INORDER : DFSTraversalOrder()
+    object POSTORDER : DFSTraversalOrder()
+}
+
+/**
+ * Perform depth-first traversal on the tree, executing [visit] on each node.
+ *
+ * **Time**: `O(n)`
+ *
+ * **Space**: `O(n)`
+ */
+fun TreeNode?.dfs(order: DFSTraversalOrder = DFSTraversalOrder.INORDER, visit: (TreeNode) -> Unit) {
+    this ?: return
+
+    when (order) {
+        DFSTraversalOrder.PREORDER -> {
+            visit(this)
+            left?.dfs(DFSTraversalOrder.PREORDER, visit)
+            right?.dfs(DFSTraversalOrder.PREORDER, visit)
+        }
+
+        DFSTraversalOrder.INORDER -> {
+            left?.dfs(DFSTraversalOrder.INORDER, visit)
+            visit(this)
+            right?.dfs(DFSTraversalOrder.INORDER, visit)
+        }
+
+        DFSTraversalOrder.POSTORDER -> {
+            left?.dfs(DFSTraversalOrder.POSTORDER, visit)
+            right?.dfs(POSTORDER, visit)
+            visit(this)
+        }
+    }
+}
+
+/**
+ * Traverse the tree (using inorder depth-first search), collecting the values
+ * into a [Collection].
+ *
+ * **Time**: `O(n)`
+ *
+ * **Space**: `O(n)`
+ */
+fun TreeNode?.collect(acc: MutableList<Int> = arrayListOf()): List<Int> =
+    this?.let {
+        left?.collect(acc)
+        acc += value
+        right?.collect(acc)
+    } ?: acc
+
+/**
+ * Traverses the tree with DFS (in the given [traversalOrder]),
+ * applies the [transform] to each [TreeNode], and
+ * collect the transform result.
+ *
+ * **Time**: `O(n)` - assuming `transform` is `<= O(n)`
+ *
+ * **Space**: `O(n)`
+ */
+fun <T> TreeNode?.collect(
+    traversalOrder: DFSTraversalOrder = INORDER,
+    acc: MutableList<T> = arrayListOf(),
+    transform: (TreeNode) -> T
+): List<T> {
+    this ?: return emptyList()
+    dfs(order = traversalOrder) { acc += transform(it) }
+    return acc
+}
+
+/**
+ * Collect the unique values to a Set.
+ *
+ * **Time**: `O(n)`
+ *
+ * **Space**: `O(n)`
+ */
+fun TreeNode?.collectUnique(acc: MutableSet<Int> = hashSetOf()): Set<Int> =
+    this?.let {
+        left?.collectUnique(acc)
+        acc += value
+        right?.collectUnique(acc)
+    } ?: acc
+
+/**
+ * Execute a breadth-first traversal of the tree.
+ *
+ * **Time**: `O(n)`
+ *
+ * **Space**: `O(n)`
+ */
+fun TreeNode?.bfs(visit: (TreeNode) -> Unit) {
+    this ?: return
+
+    val queue: Queue<TreeNode> = ArrayDeque<TreeNode>()
+    var node: TreeNode = this
+    queue.add(node)
+    while (queue.isNotEmpty()) {
+        node = queue.remove() ?: return
+        visit(node)
+        node.left?.let { queue.add(it) }
+        node.right?.let { queue.add(it) }
+    }
+}
+
+/**
+ * TODO - test
+ * Execute a depth-aware breadth-first traversal of the tree.
+ *
+ * Example usage:
+ *
+ * ```
+ * var treeLevels = hashMapOf<Int, List<Int>>()     // nodes values by depth (Map of Depth => Values)
+ * binarySearchTree.depthAwareBFS() { (node, depth) ->
+ *     treeLevels[depth] = (treeLevels[depth] ?: emptyList()) + node.value
+ * }
+ * println(treeLevels)
+ * ```
+ */
+fun TreeNode?.depthAwareBFS(visit: (Pair<TreeNode, Int>) -> Unit) {
+    this ?: return
+
+    val queue: Queue<Pair<TreeNode, Int>> = ArrayDeque<Pair<TreeNode, Int>>()
+    queue.add(Pair(this, 0))
+
+    while (queue.isNotEmpty()) {
+        val (node, depth) = queue.remove()!!
+        visit(node to depth)
+        node.left?.let {
+            queue.add(Pair(it, depth + 1))
+        }
+        node.right?.let {
+            queue.add(Pair(it, depth + 1))
+        }
+    }
+}
+
+/**
+ * Get the values in the tree by depth.
+ *
+ * **Time**: `O(n)`
+ *
+ * **Space**: `O(n)`
+ */
+fun TreeNode?.levels(): List<List<Int>> {
+    this ?: return emptyList()
+
+    val queue: Queue<Pair<TreeNode, Int>> = ArrayDeque<Pair<TreeNode, Int>>()
+    queue.add(this to 0)
+
+    val levels = arrayListOf<MutableList<Int>>()
+
+    while (queue.isNotEmpty()) {
+        val (node, depth) = queue.poll()
+
+        if (depth > levels.lastIndex) {
+            levels.add(arrayListOf())
+        }
+
+        levels[depth].add(node.value)
+
+        node.left?.let { queue.add(it to depth + 1) }
+        node.right?.let { queue.add(it to depth + 1) }
+    }
+
+    return levels
+}
+
+/**
+ * Validates that the tree has the properties of a Binary Search Tree.
+ *
+ * **Time**: `O(n)`
+ *
+ * **Space**: `O(n)`
+ */
+fun TreeNode?.isBST(validRange: IntRange = (Int.MIN_VALUE..Int.MAX_VALUE)): Boolean {
+    this ?: return true
+
+    val leftSubtreeRange = validRange.first..(value - 1)
+    val rightSubtreeRange = (value + 1)..validRange.last
+
+    return value in validRange && left.isBST(leftSubtreeRange) && right.isBST(rightSubtreeRange)
+}
+
+/**
+ * Returns a list of all root-to-leaf paths.
+ *
+ * **Time**: `O(n)`
+ *
+ * **Space**: `O(n)`
+ */
+fun TreeNode?.rootToLeafPaths(
+    currentPath: List<Int> = emptyList(),
+    paths: MutableList<List<Int>> = mutableListOf()
+): List<List<Int>> {
+    this ?: return paths
+
+    left?.rootToLeafPaths(currentPath = currentPath + value, paths = paths)
+    right?.rootToLeafPaths(currentPath = currentPath + value, paths = paths)
+
+    if (isLeaf) {
+        paths += (currentPath + value)
+    }
+    return paths
+}
+
+/**
+ * Returns a list of all (downward) allPaths. A path is defined as any sequence of nodes (length >= 1).
+ * Sequences where a descendant precedes an ancestor are not permitted.
+ *
+ * @param pathsToParent List containing all paths that end with the parent of the current node.
+ * @param allPaths List containing all downward paths.
+ *
+ * **Time**: `O(?)`
+ *
+ * **Space**: `O(?)`
+ */
+fun TreeNode?.allDownwardPaths(
+    pathsToParent: List<List<Int>> = emptyList(),
+    allPaths: MutableList<List<Int>> = mutableListOf()
+): List<List<Int>> {
+    this ?: return allPaths
+
+    val pathsToRoot = pathsToParent.map { it + value } + listOf(listOf(value))
+    allPaths += pathsToRoot
+
+    left?.allDownwardPaths(pathsToParent = pathsToRoot, allPaths = allPaths)
+    right?.allDownwardPaths(pathsToParent = pathsToRoot, allPaths = allPaths)
+
+    return allPaths
+}
+
+/**
+ * Return a list of the values in the tree. For binary search trees, the values
+ * will be sorted.
+ *
+ * **Time**: `O(n)`
+ *
+ * **Space**: `O(n)`
+ */
+fun TreeNode?.toList(): List<Int> = collect { it.value }.toList()
+
+/**
+ * Create a binary tree from the given elements.
+ * Insertion order is the same as LeetCode's 'Tree Visualizer'
+ * @param elements The values to add to the tree.
+ * @return The root of a binary tree containing the [elements]
+ * @throws IllegalArgumentException If [elements] is empty
+ */
+fun buildTree(vararg elements: Int?): TreeNode? {
+    require(elements.isNotEmpty()) { "Cannot build empty tree" }
+
+    require(elements[0] != null) { "Root cannot be null" }
+
+    val root = TreeNode(elements.first()!!)
+    var parent = root
+    val parentQueue: Queue<TreeNode> = ArrayDeque<TreeNode>()
+    parentQueue.add(parent)
+    var i = 1
+    while (i < elements.size) {
+        parent = parentQueue.remove() ?: return root
+
+        parent.left = if (elements[i] != null) TreeNode(elements[i]!!) else null
+        i++
+        parent.left?.let {
+            parentQueue.add(it)
+        }
+
+        if (i > elements.lastIndex)
+            return root
+
+        parent.right = if (elements[i] != null) TreeNode(elements[i]!!) else null
+        i++
+        parent.right?.let {
+            parentQueue.add(it)
+        }
+    }
+
+    return root
+}
+
+/**
+ * Create a binary search tree from the given elements.
+ * Insertion order is the same as LeetCode's 'Tree Visualizer'.
+ * For the BST property to be satisfied, all left subtree values must be less than the root,
+ * and all right subtree values must be greater. Duplicates are not permitted.
+ * @param elements The values to add to the tree.
+ * @return The root of a BST containing the [elements], or `null` if
+ *         elements is empty.
+ * @throws IllegalArgumentException if the elements cannot create a valid binary search tree.
+ */
+fun buildBST(vararg elements: Int?): TreeNode? {
+    val tree = buildTree(*elements)
+    require(tree.isBST()) { "The elements in the order provided violate the BST property." }
+    return tree
+}
---
+++ src/test/kotlin/datastructures/tree/binarytree/TreeNodeTest.kt
@@ -0,0 +1,334 @@
+package datastructures.tree.binarytree
+
+import org.junit.Assert.*
+import org.junit.Ignore
+import org.junit.Test
+
+class TreeNodeTest {
+
+    @Test
+    fun testToString() {
+        assertEquals("(1)", tree123.toString())
+    }
+
+    @Test
+    fun childProperties() {
+        assertTrue(tree123.hasLeft)
+        assertTrue(tree123.hasRight)
+        assertTrue(tree123.hasTwoChildren)
+        assertFalse(tree123.hasSingleChild)
+
+        val rootTree = buildTree(1)!!
+        assertFalse(rootTree.hasLeft)
+        assertFalse(rootTree.hasRight)
+        assertFalse(rootTree.hasTwoChildren)
+        assertFalse(rootTree.hasSingleChild)
+
+        val treeWithRightNode = buildTree(1, null, 3)!!
+        assertFalse(treeWithRightNode.hasLeft)
+        assertTrue(treeWithRightNode.hasRight)
+        assertFalse(treeWithRightNode.hasTwoChildren)
+        assertTrue(treeWithRightNode.hasSingleChild)
+    }
+
+    @Test
+    fun height() {
+        assertEquals(1, tree123.height)
+        assertEquals(2, tree1to5.height)
+        assertEquals(2, bst3LevelsFull.height)
+        assertEquals(3, bstWithNulls.height)
+    }
+
+
+    @Test
+    fun children() {
+        val (one, four) = bst3LevelsFull.left!!.children
+        assertEquals(1, one?.value)
+        assertEquals(4, four?.value)
+    }
+
+    @Test
+    fun find() {
+        (1..9).forEach { i ->
+            if (i in setOf(2, 7))
+                assertNull(bst3LevelsFull.find(i))
+            else
+                assertEquals(i, bst3LevelsFull.find(i)?.value)
+        }
+
+        (1..9).forEach { i ->
+            if (i in setOf(2, 5, 6))
+                assertNull(bstWithNulls.find(i))
+            else
+                assertEquals(i, bstWithNulls.find(i)?.value)
+        }
+    }
+
+    @Test
+    fun contains() {
+        (1..9).forEach { i ->
+            if (i in setOf(2, 7)) {
+                assertFalse(bst3LevelsFull.contains(i))
+                assertFalse(i in bst3LevelsFull)
+            } else {
+                assertTrue(bst3LevelsFull.contains(i))
+                assertTrue(i in bst3LevelsFull)
+            }
+        }
+
+        (1..9).forEach { i ->
+            if (i in setOf(2, 5, 6)) {
+                assertFalse(bstWithNulls.contains(i))
+                assertFalse(i in bstWithNulls)
+            } else {
+                assertTrue(bstWithNulls.contains(i))
+                assertTrue(i in bstWithNulls)
+            }
+        }
+    }
+
+    @Test
+    fun dfs() {
+        val values = arrayListOf<Int>()
+        tree123.dfs { values += it.value }
+        assertEquals(arrayListOf(2, 1, 3), values)
+
+        val bstValues = arrayListOf<Int>()
+        bst3LevelsFull.dfs { bstValues += it.value }
+        assertEquals(listOf(1, 3, 4, 5, 6, 8, 9), bstValues.toList())
+    }
+
+    @Test
+    fun dfsWithOrder() {
+        val preorderValues = arrayListOf<Int>()
+        tree1to5.dfs(order = DFSTraversalOrder.PREORDER, visit = { preorderValues += it.value })
+        assertEquals(arrayListOf(1, 2, 4, 5, 3), preorderValues)
+
+        val inorderValues = arrayListOf<Int>()
+        tree1to5.dfs(order = DFSTraversalOrder.INORDER, visit = { inorderValues += it.value })
+        assertEquals(arrayListOf(4, 2, 5, 1, 3), inorderValues)
+
+        val postorderValues = arrayListOf<Int>()
+        tree1to5.dfs(order = DFSTraversalOrder.POSTORDER, visit = { postorderValues += it.value })
+        assertEquals(arrayListOf(4, 5, 2, 3, 1), postorderValues)
+    }
+
+    @Test
+    fun bfs() {
+        val nodesTree123 = arrayListOf<Int>()
+        tree123.bfs { nodesTree123 += it.value }
+        assertEquals(listOf(1, 2, 3), nodesTree123)
+
+        val nodesTree1to5 = arrayListOf<Int>()
+        tree1to5.bfs { nodesTree1to5 += it.value }
+        assertEquals(listOf(1, 2, 3, 4, 5), nodesTree1to5)
+
+        val nodesBst3LevelsFull = arrayListOf<Int>()
+        bst3LevelsFull.bfs { nodesBst3LevelsFull += it.value }
+        assertEquals(listOf(5, 3, 8, 1, 4, 6, 9), nodesBst3LevelsFull)
+
+        val nodesBstWithNulls = arrayListOf<Int>()
+        bstWithNulls.bfs { nodesBstWithNulls += it.value }
+        assertEquals(listOf(7, 4, 9, 3, 8, 1), nodesBstWithNulls)
+    }
+
+    @Ignore
+    @Test
+    fun depthAwareBFS() {
+        // TODO
+    }
+
+    @Test
+    fun levels() {
+        assertEquals(listOf(listOf(1), listOf(2, 3)), tree123.levels())
+        assertEquals(
+            listOf(
+                listOf(1),
+                listOf(2, 3),
+                listOf(4, 5)
+            ),
+            tree1to5.levels()
+        )
+        assertEquals(
+            listOf(
+                listOf(5),
+                listOf(3, 8),
+                listOf(1, 4, 6, 9)
+            ),
+            bst3LevelsFull.levels()
+        )
+    }
+
+    @Test
+    fun collect() {
+        assertEquals(listOf(2, 1, 3), tree123.collect())
+        assertEquals(listOf(1, 3, 4, 5, 6, 8, 9), bst3LevelsFull.collect())
+    }
+
+    @Test
+    fun collectWithTransform() {
+        assertEquals(listOf(1, 3, 4, 5, 6, 8, 9).map { it * 2 },
+            bst3LevelsFull.collect { it.value * 2 })
+    }
+
+    @Test
+    fun collectUnique() {
+        assertEquals(setOf(2, 1, 3), tree123.collectUnique())
+        assertEquals(setOf(1, 3, 4, 5, 6, 8, 9), bst3LevelsFull.collectUnique())
+        assertEquals(setOf(1, 2, 3, 4, 5), buildTree(1, 2, 3, 2, 3, 2, 3, 1, 3, 4, 5).collectUnique())
+    }
+
+    @Test
+    fun rootToLeafPaths() {
+        assertEquals(listOf(listOf(1, 2), listOf(1, 3)), tree123.rootToLeafPaths())
+        assertEquals(
+            listOf(listOf(5, 3, 1), listOf(5, 3, 4), listOf(5, 8, 6), listOf(5, 8, 9)),
+            bst3LevelsFull.rootToLeafPaths()
+        )
+        assertEquals(listOf(listOf(7, 4, 3, 1), listOf(7, 9, 8)), bstWithNulls.rootToLeafPaths())
+    }
+
+    @Test
+    fun allDownwardPaths() {
+        val nullTree: TreeNode? = null
+        assertEquals(emptyList<List<Int>>(), nullTree.allDownwardPaths())
+
+        val expected123 = listOf(listOf(1), listOf(2), listOf(1, 2), listOf(3), listOf(1, 3))
+        val actual123 = tree123.allDownwardPaths()
+        assertEquals(expected123.size, actual123.size)
+        assertEquals(expected123.toSet(), actual123.toSet())
+
+        val expectedBST = listOf(
+            listOf(5),
+            listOf(3),
+            listOf(5, 3),
+            listOf(1),
+            listOf(3, 1),
+            listOf(5, 3, 1),
+            listOf(4),
+            listOf(3, 4),
+            listOf(5, 3, 4),
+            listOf(8),
+            listOf(5, 8),
+            listOf(6),
+            listOf(8, 6),
+            listOf(5, 8, 6),
+            listOf(9),
+            listOf(8, 9),
+            listOf(5, 8, 9)
+        )
+        val actualBST = bst3LevelsFull.allDownwardPaths()
+        assertEquals(expectedBST.size, actualBST.size)
+        assertEquals(expectedBST.toSet(), actualBST.toSet())
+
+        val expectedBSTWithNulls = listOf(
+            listOf(7),
+            listOf(4),
+            listOf(7, 4),
+            listOf(3),
+            listOf(4, 3),
+            listOf(7, 4, 3),
+            listOf(1),
+            listOf(3, 1),
+            listOf(4, 3, 1),
+            listOf(7, 4, 3, 1),
+            listOf(9),
+            listOf(7, 9),
+            listOf(8),
+            listOf(9, 8),
+            listOf(7, 9, 8)
+        )
+        val actualBSTWithNulls = bstWithNulls.allDownwardPaths()
+        assertEquals(expectedBSTWithNulls.size, actualBSTWithNulls.size)
+        assertEquals(expectedBSTWithNulls.toSet(), actualBSTWithNulls.toSet())
+    }
+
+    @Test
+    fun buildTree() {
+        val tree = buildTree(1, 2, 3)
+        assertNotNull(tree)
+        assertEquals(3, tree?.size)
+        assertEquals(1, tree?.value)
+        assertEquals(2, tree?.left?.value)
+        assertEquals(3, tree?.right?.value)
+
+        val tree2 = buildTree(5, 3, 8, 1, 4, 6, 9)
+        assertNotNull(tree2)
+        assertEquals(7, tree2?.size)
+        assertEquals(bst3LevelsFull, tree2)
+
+        val tree3 = buildTree(1, null, 1, null, 1, 2)
+        assertNotNull(tree3)
+        assertEquals(4, tree3?.size)
+        assertEquals(treeUnbalanced, tree3)
+
+        val tree4 = buildTree(1, null, 1, null, 1, null, 1, null, 1, null, 1, null, 1, null, 1, null, 1, null, 1, null, 1, 2)
+        assertEquals(12, tree4?.size)
+        assertEquals(treeUnbalanced2, tree4)
+
+        val tree5 = buildTree(7, null, 8, 9, 10)
+        assertEquals(4, tree5?.size)
+        val expected = TreeNode(7).apply {
+            right = TreeNode(8).apply {
+                left = TreeNode(9)
+                right = TreeNode(10)
+            }
+        }
+        assertEquals(expected, tree5)
+    }
+
+    @Test
+    fun buildTreeWithNulls() {
+        val tree = buildTree(1, null, 2, 3)
+        assertNotNull(tree)
+        assertEquals(3, tree?.size)
+        assertEquals(1, tree?.value)
+        assertNull(tree?.left?.value)
+        assertEquals(2, tree?.right?.value)
+        assertEquals(3, tree?.right?.left?.value)
+        assertNull(tree?.right?.left?.right)
+
+        val tree2 = buildTree(7, 4, 9, 3, null, 8, null, 1)
+        assertEquals(bstWithNulls, tree2)
+        assertEquals(6, tree2?.size)
+        assertEquals(7, tree2?.value)
+        assertEquals(4, tree2?.left?.value)
+        assertEquals(9, tree2?.right?.value)
+        assertEquals(3, tree2?.left?.left?.value)
+        assertEquals(1, tree2?.left?.left?.left?.value)
+        assertEquals(8, tree2?.right?.left?.value)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `buildTree throws exception if root is null`() {
+        buildTree(null)
+    }
+
+    @Test
+    fun buildBST() {
+        val tree1 = buildBST(2, 1, 3)
+        assertNotNull(tree1)
+        assertEquals(3, tree1?.size)
+        assertEquals(2, tree1?.value)
+        assertEquals(1, tree1?.left?.value)
+        assertEquals(3, tree1?.right?.value)
+
+        val tree2 = buildBST(7, 4, 9, 3, null, 8, null, 1)
+        assertEquals(bstWithNulls, tree2)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `buildBST throws exception if root is null`() {
+        buildBST(null)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `buildBST throws exception if elements violate BST property`() {
+        buildBST(1, 2, 3)
+    }
+
+    @Test
+    fun toList() {
+        assertEquals(listOf(1, 3, 4, 5, 6, 8, 9), bst3LevelsFull.toList())
+    }
+}
