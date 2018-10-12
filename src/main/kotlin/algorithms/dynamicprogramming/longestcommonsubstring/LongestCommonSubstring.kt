1fef879d80f1b68e6ddbd0ac6b5079847555254f
Navid Rojiani <nrojiani@gmail.com>
2018-10-11 22:46:24 -0400

    DP: Longest Common Substring

---
+++ src/main/kotlin/algorithms/dynamicprogramming/longestcommonsubstring/LongestCommonSubstring.kt
@@ -0,0 +1,81 @@
+package algorithms.dynamicprogramming.longestcommonsubstring
+
+import kotlin.math.max
+
+/**
+ * [Wiki - Longest Common Substring Problem](https://en.wikipedia.org/wiki/Longest_common_substring_problem)
+ */
+
+/**
+ * Returns the length of the longest common substring.
+ */
+fun lengthOfLongestCommonSubstring(a: String, b: String): Int {
+    val dp = Array(a.length + 1) { IntArray(b.length + 1) }
+    var maxLength = 0
+
+    // Leave dp[0][j] and dp[i][0] (first row and column) as zeroes
+    (1..a.length).forEach { i ->
+        (1..b.length).forEach { j ->
+            if (a[i - 1] == b[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+                maxLength = max(maxLength, dp[i][j])
+            } // otherwise, leave dp[i][j] = 0
+        }
+    }
+
+    return maxLength
+}
+
+/**
+ * This version returns the longest common substring itself.
+ */
+fun longestCommonSubstring(a: String, b: String): String {
+    val dp = Array(a.length + 1) { IntArray(b.length + 1) }
+    var maxLength = 0
+    var endIndex = 0
+
+    (1..a.length).forEach { i ->
+        (1..b.length).forEach { j ->
+            if (a[i - 1] == b[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+
+                if (dp[i][j] > maxLength) {
+                    maxLength = dp[i][j]
+                    endIndex = i
+                }
+            }
+        }
+    }
+
+    println(solutionGrid(a, b, dp))
+
+    return a.substring((endIndex - maxLength) until endIndex)
+}
+
+/**
+ * Return the solution grid as a String:
+ * ```
+ * |   | X | B | A | B | A |
+ * | X | 0 | 0 | 0 | 0 | 0 |
+ * | A | 0 | 0 | 1 | 0 | 1 |
+ * | B | 0 | 1 | 0 | 2 | 0 |
+ * | A | 0 | 0 | 2 | 0 | 3 |
+ * | B | 0 | 1 | 0 | 3 | 0 |
+ * ```
+ */
+private fun solutionGrid(a: String, b: String, grid: Array<IntArray>): String =
+    with (StringBuilder()) {
+        // Top Header Row
+        append("|   | X | ")
+        if (b.isNotEmpty())
+            append(b.toList().joinToString(" | ") + " |") // rest of top header row
+        append("\n")
+
+        // Rows with left header column
+        val rowStart: (Int) -> String = { i -> if (i == 0) "| X | " else "| " + a[i - 1] + " | " }
+        (0..a.length).forEach { i ->
+            append(rowStart(i))
+            append(grid[i].joinToString(" | ") + " |\n")
+        }
+        toString()
+    }
---
+++ src/test/kotlin/algorithms/dynamicprogramming/longestcommonsubstring/LongestCommonSubstringTest.kt
@@ -0,0 +1,40 @@
+package algorithms.dynamicprogramming.longestcommonsubstring
+
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class LongestCommonSubstringTest {
+
+    @Test
+    fun longestCommonSubstring() {
+        assertEquals("", longestCommonSubstring("", ""))
+        assertEquals("", longestCommonSubstring("A", ""))
+        assertEquals("", longestCommonSubstring("", "A"))
+        assertEquals("A", longestCommonSubstring("A", "A"))
+        assertEquals("A", longestCommonSubstring("ABC", "A"))
+        assertEquals("AB", longestCommonSubstring("ABC", "BABA"))
+        assertEquals("ABCABC", longestCommonSubstring("ABCABCA", "CABCABC"))
+        assertEquals("ABCABC", longestCommonSubstring("CABCABC", "ABCABCA"))
+        assertEquals("abcd", longestCommonSubstring("abcdxyz", "xyzabcd"))
+        assertEquals("abcdez", longestCommonSubstring("zxabcdezy", "yzabcdezx"))
+        assertEquals("ABC", longestCommonSubstring("ABC", "ABCD"))
+        assertTrue(longestCommonSubstring("ABAB", "BABA") in setOf("ABA", "BAB"))
+    }
+
+    @Test
+    fun lengthOfLongestCommonSubstring() {
+        assertEquals(0, lengthOfLongestCommonSubstring("", ""))
+        assertEquals(0, lengthOfLongestCommonSubstring("A", ""))
+        assertEquals(0, lengthOfLongestCommonSubstring("", "A"))
+        assertEquals(1, lengthOfLongestCommonSubstring("A", "A"))
+        assertEquals(1, lengthOfLongestCommonSubstring("ABC", "A"))
+        assertEquals(2, lengthOfLongestCommonSubstring("ABC", "BABA"))
+        assertEquals(6, lengthOfLongestCommonSubstring("ABCABCA", "CABCABC"))
+        assertEquals(6, lengthOfLongestCommonSubstring("CABCABC", "ABCABCA"))
+        assertEquals(4, lengthOfLongestCommonSubstring("abcdxyz", "xyzabcd"))
+        assertEquals(6, lengthOfLongestCommonSubstring("zxabcdezy", "yzabcdezx"))
+        assertEquals(3, lengthOfLongestCommonSubstring("ABC", "ABCD"))
+        assertEquals(3, lengthOfLongestCommonSubstring("ABAB", "BABA"))
+    }
+}
